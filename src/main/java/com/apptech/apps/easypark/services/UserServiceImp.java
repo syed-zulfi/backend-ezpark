9657b782d499715feb39f38d5aed227c28a7db1a
Syed Zulfikhar Ahmed <Zulfikhar@Zulfikhar-PC>
2018-01-10 22:00:23 +0530

    code format of service and minor update in soap project file

--- src/main/java/com/apptech/apps/easypark/services/UserServiceImp.java
+++ src/main/java/com/apptech/apps/easypark/services/UserServiceImp.java
@@ -1,6 +1,5 @@
 package com.apptech.apps.easypark.services;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +24,7 @@ import com.apptech.apps.easypark.util.ResponseUtil;
 @Component
 public class UserServiceImp implements UserService {
 
-	private static final Logger log = LoggerFactory
-			.getLogger(UserServiceImp.class);
+	private static final Logger log = LoggerFactory.getLogger(UserServiceImp.class);
 	private UserDAO userDao;
 	private UserRepo userRepo;
 
@@ -50,65 +48,48 @@ public class UserServiceImp implements UserService {
 			userRepo.userIdExists(user.getUsername());
 			log.info("User not found in the records, registering the user");
 			userRepo.createNewUser(User.buildDomainObject(user));
-			_resDto = ResponseUtil.createResponseDTO(ReturnCode.REGESTERED,
-					user,false);
+			_resDto = ResponseUtil.createResponseDTO(ReturnCode.REGESTERED, user, false);
 			log.info("User registered completed");
 
 		} catch (UserExistException e) {
-			_resDto = ResponseUtil.createResponseDTO(ReturnCode.DECLINED, user,true);
-			_resDto = ResponseUtil.createResponseDTO(
-					ReturnCode.DECLINED, user,true);
+			_resDto = ResponseUtil.createResponseDTO(ReturnCode.DECLINED, user, true);
 			_resDto.createError();
-			_resDto.getError().setErrorCode(
-					ReturnCode.DECLINED.message());
-			log.error("User already available in the records hence registration process declined for this user"
-					+ e);
+			_resDto.getError().setErrorCode(ReturnCode.DECLINED.message());
+			log.error("User already available in the records hence registration process declined for this user" + e);
 		} catch (ObjectCreationException | ApplicationException e) {
 			log.error("Some error occured during registratoin process" + e);
-			_resDto = ResponseUtil.createResponseDTO(
-					ReturnCode.APPLICATION_ERROR, user,true);
+			_resDto = ResponseUtil.createResponseDTO(ReturnCode.APPLICATION_ERROR, user, true);
 			_resDto.createError();
-			_resDto.getError().setErrorCode(
-					ReturnCode.APPLICATION_ERROR.message());
+			_resDto.getError().setErrorCode(ReturnCode.APPLICATION_ERROR.message());
 			_resDto.getError().setErrorDescription(e.getMessage());
 		} catch (AccessDeniedException e) {
 			log.error("Some error occured during registratoin process" + e);
-			_resDto = ResponseUtil.createResponseDTO(
-					ReturnCode.FORBID_ACCESS, user,true);
+			_resDto = ResponseUtil.createResponseDTO(ReturnCode.FORBID_ACCESS, user, true);
 			_resDto.createError();
-			_resDto.getError().setErrorCode(
-					ReturnCode.FORBID_ACCESS.message());
+			_resDto.getError().setErrorCode(ReturnCode.FORBID_ACCESS.message());
 			_resDto.getError().setErrorDescription(e.getMessage());
 		}
 		return _resDto;
 	}
 
-/*	@Override
-	public ResponseDTO registerUser(UserDTO user) {
-		ResponseDTO _resDto = null;
-		try {
-			userDao.userIdExists(user.getUsername());
-			log.info("User not found in the records, registering the user");
-			userDao.createNewUser(User.buildDomainObject(user));
-			user.setPassword("");
-			_resDto = ResponseUtil.createResponseDTO(ReturnCode.REGESTERED,
-					user);
-			log.info("User registered witht he applciiation");
-		} catch (ObjectCreationException | ApplicationException e) {
-			log.error("Some error occured during registratoin process" + e);
-			_resDto = ResponseUtil.createResponseDTO(
-					ReturnCode.APPLICATION_ERROR, user);
-			_resDto.createError();
-			_resDto.getError().setErrorCode(
-					ReturnCode.APPLICATION_ERROR.message());
-		} catch (UserExistException e) {
-			_resDto = ResponseUtil.createResponseDTO(ReturnCode.DECLINED, user);
-			log.error("User already available in the records hence registration process declined for this user"
-					+ e);
-		}
-		return _resDto;
-	}
-*/
+	/*
+	 * @Override public ResponseDTO registerUser(UserDTO user) { ResponseDTO
+	 * _resDto = null; try { userDao.userIdExists(user.getUsername()); log.info(
+	 * "User not found in the records, registering the user");
+	 * userDao.createNewUser(User.buildDomainObject(user));
+	 * user.setPassword(""); _resDto =
+	 * ResponseUtil.createResponseDTO(ReturnCode.REGESTERED, user); log.info(
+	 * "User registered witht he applciiation"); } catch
+	 * (ObjectCreationException | ApplicationException e) { log.error(
+	 * "Some error occured during registratoin process" + e); _resDto =
+	 * ResponseUtil.createResponseDTO( ReturnCode.APPLICATION_ERROR, user);
+	 * _resDto.createError(); _resDto.getError().setErrorCode(
+	 * ReturnCode.APPLICATION_ERROR.message()); } catch (UserExistException e) {
+	 * _resDto = ResponseUtil.createResponseDTO(ReturnCode.DECLINED, user);
+	 * log.error(
+	 * "User already available in the records hence registration process declined for this user"
+	 * + e); } return _resDto; }
+	 */
 	@Override
 	public ResponseDTO loginUser(String uname, String pwd) {
 		ResponseDTO _resDto = ResponseDTO.create();
@@ -136,26 +117,21 @@ public class UserServiceImp implements UserService {
 		ResponseDTO rdto = null;
 		try {
 			if (type.equals(Fields.EMAIL.getType())) {
-				//userDao.emailExist(field);
+				// userDao.emailExist(field);
 				userRepo.emailExist(field);
-				rdto = ResponseUtil.createResponseDTO(
-						ReturnCode.EMAIL_AVAILABLE, field,true);
+				rdto = ResponseUtil.createResponseDTO(ReturnCode.EMAIL_AVAILABLE, field, true);
 			} else if (type.equals(Fields.USERID.getType())) {
-				//userDao.userIdExists(field);
+				// userDao.userIdExists(field);
 				userRepo.userIdExists(field);
-				rdto = ResponseUtil.createResponseDTO(ReturnCode.UID_AVAILABLE,
-						field,true);
+				rdto = ResponseUtil.createResponseDTO(ReturnCode.UID_AVAILABLE, field, true);
 			} else {
-				rdto = ResponseUtil.createResponseDTO(ReturnCode.BAD_INPUT,
-						type,true);
+				rdto = ResponseUtil.createResponseDTO(ReturnCode.BAD_INPUT, type, true);
 			}
 		} catch (UserExistException e) {
 			if (e.getField().equals(Fields.EMAIL)) {
-				rdto = ResponseUtil.createResponseDTO(
-						ReturnCode.EMAIL_DUPLICATE, field,true);
+				rdto = ResponseUtil.createResponseDTO(ReturnCode.EMAIL_DUPLICATE, field, true);
 			} else {
-				rdto = ResponseUtil.createResponseDTO(
-						ReturnCode.UID_UNAVAILABLE, field,true);
+				rdto = ResponseUtil.createResponseDTO(ReturnCode.UID_UNAVAILABLE, field, true);
 			}
 			e.printStackTrace();
 		}
